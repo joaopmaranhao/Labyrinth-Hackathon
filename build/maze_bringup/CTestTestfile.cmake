052d2cebee9f14593624bdb4a99c61e68539ad46
Joao Pedro <blvck.joao37@gmail.com>
2026-06-05 23:37:10 -0300

    build: colcon build brabo

---
+++ build/maze_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/joao-pedro/git/Labyrinth-Hackathon/src/maze_bringup
+# Build directory: /home/joao-pedro/git/Labyrinth-Hackathon/build/maze_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/maze_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/joao-pedro/git/Labyrinth-Hackathon/src/maze_robot
+# Build directory: /home/joao-pedro/git/Labyrinth-Hackathon/build/maze_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

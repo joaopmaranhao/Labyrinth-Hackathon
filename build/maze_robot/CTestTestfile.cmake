169e243427f74b39aaed2822e65542ef5429ee9d
João Maranhão <blvck.joao37@gmail.com>
2026-06-16 15:04:47 -0300

    Delete build directory

--- build/maze_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joao-pedro/git/Labyrinth-Hackathon/src/maze_robot
-# Build directory: /home/joao-pedro/git/Labyrinth-Hackathon/build/maze_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
